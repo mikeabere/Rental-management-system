aaa4b7cbd7ecb6c1194393c97bc8b7eef9dce23b
mike mwanzi <mikeabere@gmail.com>
2024-06-17 21:30:47 +0300

    created routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,38 @@
-import { useState } from 'react'
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Error, Login, Register, Landing, HomeLayout } from "./pages";
 
+const router = createBrowserRouter([
+{
+  path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+        
+      },
+      {
+        path: "login",
+        element: <Login />,
+       
+      },
+    ],
+},
 
-
+]);
 
 function App() {
- 
-
   return (
-    <>
-     <div></div>
-    </>
-  )
+  
+    <RouterProvider router={router} />
+);
+
 }
 
-export default App
+export default App;
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,2 +1,5 @@
 export { default as Register } from "./Register";
 export { default as Login } from "./Login";
+export { default as HomeLayout } from "./HomeLayout";
+export { default as Landing } from "./Landing";
+export { default as Error } from "./Error";
