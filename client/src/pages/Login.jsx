746707fcce802f3e2097c935434168c0f95bcfd6
mike mwanzi <mikeabere@gmail.com>
2024-06-18 18:56:40 +0300

    Did Login and Register components

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React from "react";
+import { Link, Form, redirect, useNavigate } from "react-router-dom";
+import { Logo, FormRow, SubmitBtn } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+
+
+
 
 function Login() {
+  const navigate = useNavigate();
+  const loginDemoUser = async () => {
+    const data = {
+      email: "test@test.com",
+      password: "secret123",
+    };
+    try {
+      await customFetch.post("/auth/login", data);
+      toast.success("take a test drive");
+      navigate("/dashboard");
+    } catch (error) {
+      toast.error(error?.response?.data?.msg);
+    }
+  };
   return (
-    <div>Login</div>
-  )
+    <Wrapper>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Login</h4>
+        <FormRow type="email" name="email" />
+        <FormRow type="password" name="password" />
+        <SubmitBtn />
+        <button type="button" className="btn btn-block" onClick={loginDemoUser}>
+          explore the app
+        </button>
+        <p>
+          Not a member yet?
+          <Link to="/register" className="member-btn">
+            Register
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default Login
+export default Login;
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
+import { Logo, FormRow, SubmitBtn } from "../components";
+import { Form, Link } from "react-router-dom";
+
+
 
 function Register() {
   return (
-    <div>Register</div>
-  )
+    <Wrapper>
+      <Form method="post" className="form">
+        <Logo />
+        <h4>Register</h4>
+        <FormRow type="text" name="name" />
+        <FormRow type="text" name="lastName" labelText="last name" />
+        <FormRow type="text" name="location" />
+        <FormRow type="email" name="email" />
+
+        <FormRow type="password" name="password" />
+
+        <SubmitBtn />
+        <p>
+          Already a member?
+          <Link to="/login" className="member-btn">
+            Login
+          </Link>
+        </p>
+      </Form>
+    </Wrapper>
+  );
 }
 
-export default Register
+export default Register;
