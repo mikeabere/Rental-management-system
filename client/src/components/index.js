599dbd1366117f4c66bcb8e87748900c28cea057
mike mwanzi <mikeabere@gmail.com>
2024-07-10 21:48:53 +0300

    exported Bigsidebar and Smallsidebar

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Logo } from "./Logo";
 export { default as FormRow } from "./FormRow";
 export { default as SubmitBtn } from "./SubmitBtn";
+export { default as SmallSidebar } from "./SmallSidebar";
+export { default as BigSidebar } from "./BigSidebar";
