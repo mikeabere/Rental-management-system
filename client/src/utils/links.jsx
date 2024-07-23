10c9f7c00a1bfb3a897cece9fdc9a6e762258abe
mike mwanzi <mikeabere@gmail.com>
2024-07-23 21:51:01 +0300

    updated links.jsx

--- client/src/utils/links.jsx
+++ client/src/utils/links.jsx
@@ -7,8 +7,8 @@ import { ImProfile } from "react-icons/im";
 import { MdAdminPanelSettings } from "react-icons/md";
 
 const links = [
-  { text: "add job", path: ".", icon: <FaWpforms /> },
-  { text: "all jobs", path: "all-jobs", icon: <MdQueryStats /> },
+  { text: "units", path: ".", icon: <FaWpforms /> },
+  { text: "landlords", path: "all-jobs", icon: <MdQueryStats /> },
   { text: "stats", path: "stats", icon: <IoBarChartSharp /> },
   { text: "profile", path: "profile", icon: <ImProfile /> },
   { text: "admin", path: "admin", icon: <MdAdminPanelSettings /> },
