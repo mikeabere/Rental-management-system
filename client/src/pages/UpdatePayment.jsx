c53bb30c8b58d2e7594d1f95d9ecd9d36d4ad349
mike mwanzi <mikeabere@gmail.com>
2024-11-24 22:58:19 +0300

    update to updatepayment and withdrawfunds component

---
+++ client/src/pages/UpdatePayment.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function UpdatePayment() {
+  return (
+    <div>UpdatePayment</div>
+  )
+}
+
+export default UpdatePayment
---
+++ client/src/pages/WithdrawFunds.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function WithdrawFunds() {
+  return (
+    <div>WithdrawFunds</div>
+  )
+}
+
+export default WithdrawFunds
