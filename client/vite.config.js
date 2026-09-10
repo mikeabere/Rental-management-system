0408a97adbecf2cd7e22a0ed9956ac1925d3ef4c
mike mwanzi <mikeabere@gmail.com>
2026-09-10 23:28:57 +0300

    cumulative changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:6000/api",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
-});
+import { defineConfig } from 'vite';
+ import react from '@vitejs/plugin-react';
+  export default defineConfig({plugins:[react()],
+    server:{port:5173,
+      proxy:{'/api':'http://localhost:5000'}}});
