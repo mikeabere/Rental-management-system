dfe7c192f5bf78e2001d4d58e3730fb635c43eaa
mike mwanzi <mikeabere@gmail.com>
2026-09-23 18:54:59 +0300

    Add BrowserRouter and RouterProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,74 @@
 import React,{useEffect,useState} from 'react'; 
+import {BrowserRouter, RouterProvider} from 'react-router-dom';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Landing />,
+      },
+      {
+        path: "register",
+        element: <Register />,
+        action: registerAction,
+      },
+      {
+        path: "login",
+        element: <Login />,
+        action: loginAction(queryClient),
+      },
+      {
+        path: "dashboard",
+        element: <DashboardLayout queryClient={queryClient} />,
+        loader: dashboardLoader(queryClient),
+        children: [
+          {
+            index: true,
+            element: <AddJob />,
+            action: addJobAction(queryClient),
+          },
+          {
+            path: "stats",
+            element: <Stats />,
+            loader: statsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
+          {
+            path: "all-jobs",
+            element: <AllJobs />,
+            loader: allJobsLoader(queryClient),
+            errorElement: <ErrorElement />,
+          },
 
-
- 
-   
+          {
+            path: "profile",
+            element: <Profile />,
+            action: profileAction(queryClient),
+          },
+          {
+            path: "admin",
+            element: <Admin />,
+            loader: adminLoader,
+          },
+          {
+            path: "edit-job/:id",
+            element: <EditJob />,
+            loader: editJobLoader(queryClient),
+            action: editJobAction(queryClient),
+          },
+          {
+            path: "delete-job/:id",
+            action: deleteJobAction(queryClient),
+          },
+        ],
+      },
+    ],
+  },
+]);
 
 const api=async(path,options={})=>{
 
